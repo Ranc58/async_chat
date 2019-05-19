e16c4dc2a5a71f1fde1760b83c1d4ccc03950af5
Ranc58 <rancvova@gmail.com>
2019-05-19 15:09:13 +0300

    Fix lof file open. Some fixes for exceptions handling

--- chat_tool.py
+++ chat_tool.py
@@ -1,17 +1,12 @@
 import asyncio
 import json
 import logging
+import sys
+from contextlib import asynccontextmanager
 from datetime import datetime
 import socket
 
 from aiofile import AIOFile
-
-
-async def write_to_file(text, history_log_path):
-    async with AIOFile(f'{history_log_path}/history_logs.txt', 'a+') as afp:
-        current_datetime = datetime.now().strftime('%d.%m.%y %H:%M')
-        text_to_output = f'[{current_datetime}] {text}\n'
-        await afp.write(text_to_output)
 
 
 class ChatTool:
@@ -21,74 +16,120 @@ class ChatTool:
         self.port = port
         self.history_log_path = history_log_path
         self.attempts = attempts
+        self.reader = None
+        self.writer = None
+        self.log_file = None
+
+    async def write_to_file(self, text):
+        current_datetime = datetime.now().strftime('%d.%m.%y %H:%M')
+        text_to_output = f'[{current_datetime}] {text}\n'
+        await self.log_file.write(text_to_output)
+
+    async def read_message_from_chat(self):
+        try:
+            response = await self.reader.readline()
+
+        except (
+                socket.gaierror,
+                ConnectionRefusedError,
+                ConnectionResetError,
+                ConnectionError,
+        ) as error:
+            raise error
+        else:
+            decoded_data = response.decode().rstrip('\n\r')
+            logging.debug(decoded_data)
+            return decoded_data
+
+    async def write_message_to_chat(self, message=None):
+
+        if not message:
+            message = f'\n'
+        try:
+            self.writer.write(message.encode())
+        except (
+                socket.gaierror,
+                ConnectionRefusedError,
+                ConnectionResetError,
+                ConnectionError,
+        ) as error:
+            raise error
+
+    @asynccontextmanager
+    async def reader_writer_tools(self):
+        try:
+            self.log_file = await AIOFile(f'{self.history_log_path}/history_logs.txt', 'a+')
+            await self.log_file.open()
+            await self.get_connection_tools()
+            yield
+        finally:
+            await self.log_file.close()
+            if self.writer:
+                self.writer.close()
 
     async def get_connection_tools(self):
         attempts_count = 0
-        reader = None
-        writer = None
-        while not reader:
+
+        while not self.reader:
             try:
-                reader, writer = await asyncio.open_connection(self.host, self.port)
+                self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
                 success_connect_msg = 'Соединение установлено'
                 logging.debug(success_connect_msg)
-                await write_to_file(success_connect_msg, self.history_log_path)
+                await self.write_to_file(success_connect_msg)
             except (
                     socket.gaierror,
                     ConnectionRefusedError,
                     ConnectionResetError,
+                    ConnectionError,
             ):
-                if attempts_count < self.attempts:
+                if attempts_count < int(self.attempts):
                     error_msg = 'Нет соединения. Повторная попытка.'
                     logging.debug(error_msg)
-                    await write_to_file(error_msg, self.history_log_path)
+                    await self.write_to_file(error_msg)
                     attempts_count += 1
-                    reader = None
                     continue
                 else:
                     error_msg = 'Нет соединения. Повторная попытка через 3 сек.'
                     logging.debug(error_msg)
-                    await write_to_file(error_msg, self.history_log_path)
-                    reader = None
+                    await self.write_to_file(error_msg)
                     await asyncio.sleep(3)
                     continue
-        return reader, writer
 
     async def read_chat(self):
-        reader, _ = await self.get_connection_tools()
-        while True:
-            data = await reader.readline()
-            decoded_data = data.decode().rstrip('\n\r')
-            logging.debug(decoded_data)
-            await write_to_file(decoded_data, self.history_log_path)
+        async with self.reader_writer_tools():
+            while True:
+                try:
+                    data = await self.reader.readline()
+                except (
+                        socket.gaierror,
+                        ConnectionRefusedError,
+                        ConnectionResetError,
+                        ConnectionError,
+                ):
+                    await self.get_connection_tools()
+                    data = await self.reader.readline()
+                decoded_data = data.decode().rstrip('\n\r')
+                logging.debug(decoded_data)
+                await self.write_to_file(decoded_data)
 
-    async def _register(self,  writer, reader, username):
-        response = await reader.readline()
-        decoded_data = response.decode().rstrip('\n\r')
-        logging.debug(decoded_data)
-        writer.write(f'\n'.encode())
-        response = await reader.readline()
-        decoded_data = response.decode().rstrip('\n\r')
-        logging.debug(decoded_data)
+    async def _register(self, username):
+        await self.read_message_from_chat()
+        await self.write_message_to_chat()
+        await self.read_message_from_chat()
         if username:
-            writer.write(f'{username}\n'.encode())
+            await self.write_message_to_chat(f'{username}\n')
             logging.debug(f'SENDED: {username}')
         else:
-            writer.write('\n'.encode())
-        response = await reader.readline()
-        decoded_data = response.decode().rstrip('\n\r')
-        logging.debug(decoded_data)
-        writer.close()
+            await self.write_message_to_chat()
+        decoded_data = await self.read_message_from_chat()
         return json.loads(decoded_data)
 
-    async def _authorise(self, token, writer, reader):
+    async def _authorise(self, token):
         success = True
-        response = await reader.readline()
-        decoded_data = response.decode().rstrip('\n\r')
-        logging.debug(decoded_data)
-        writer.write(f'{token}\n'.encode())
+        await self.read_message_from_chat()
+        await self.write_message_to_chat(f'{token}\n')
         logging.debug(f'SENDED: {token}')
-        response = await reader.readline()
-        decoded_data = response.decode().rstrip('\n\r')
+        decoded_data = await self.read_message_from_chat()
         if not json.loads(decoded_data):
             logging.debug('incorrect token')
             success = False
@@ -96,25 +137,31 @@ class ChatTool:
             logging.debug(decoded_data)
         return success
 
-    async def _submit_message(self, message, writer):
-        message = message.replace('\n', '')
-        writer.write(f'{message}\n\n'.encode())
+    async def _submit_message(self, message):
+
+        await self.write_message_to_chat(f'{message}\n\n')
         logging.debug(f'SENDED: {message}')
 
     async def write_to_chat(self, message, token=None, username=None):
-        reader, writer = await self.get_connection_tools()
 
-        if not token:
-            registred_data = await self._register(writer, reader, username)
-            token = registred_data.get('account_hash')
-            reader, writer = await self.get_connection_tools()
-
-        authorised = await self._authorise(token, writer, reader)
-
-        if not authorised:
-            writer.close()
-            return
-
-        await self._submit_message(message, writer)
-        writer.close()
-
+        async with self.reader_writer_tools():
+            try:
+                if not token:
+                    registered_data = await self._register(username)
+                    token = registered_data.get('account_hash')
+                    # for reconnect by token
+                    self.reader = None
+                    self.writer.close()
+                    await self.get_connection_tools()
+                authorised = await self._authorise(token)
+                if not authorised:
+                    return
+                await self._submit_message(message)
+            except (
+                    socket.gaierror,
+                    ConnectionRefusedError,
+                    ConnectionResetError,
+                    ConnectionError,
+            ) as error:
+                logging.error(str(error))
+                sys.exit()
